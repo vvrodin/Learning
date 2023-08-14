5c0839c47e3ea2a6ff87a1445b15f7ece72052a9
vvrodin <vova.rodin@bk.ru>
2023-08-15 02:04:24 +0300

    second commit

--- Algorithms and data structure/Linked list data structure.py
+++ Algorithms and data structure/Linked list data structure.py
@@ -5,29 +5,16 @@ class Node:
         else:
             self.data = list(*args)
         self.next_node = None
+        self.previous_node = None
         return
 
     def __str__(self):
         return str(self.data)
 
 
-class Linked_list:
+class linked_lists:
     def __init__(self):
-        self.head = Node(None, first_init=True)
-        self.head.next_node = Node(None, first_init=True)
-
-    def add(self, *args):
-        args = list(args)
-        if self.head.data is None:
-            self.head.data = args
-            return None
-        elif self.head.next_node.data is None:
-            self.head.next_node.data = args
-            return None
-        node = self.head
-        while node.next_node is not None:
-            node = node.next_node
-        node.next_node = Node(args)
+        self.head = None
 
     def find(self, *element):
         element = list(element)
@@ -38,23 +25,6 @@ class Linked_list:
             return node
         if node.data != element:
             return -1
-
-    def remove(self, *element):
-        element = list(element)
-        node = self.head
-        previous_node = -1
-        while node.data != element and node.next_node is not None:
-            previous_node = node
-            node = node.next_node
-        if node.data == element and node.next_node is None:
-            previous_node.next_node = None
-        if node.next_node is None and node.data != element:
-            raise KeyError
-        if previous_node == -1:
-            self.head = self.head.next_node
-            return
-        previous_node.next_node = node.next_node
-        return
 
     def count(self, *element):
         counter = 0
@@ -74,3 +44,100 @@ class Linked_list:
             print(node, end=' - ')
             node = node.next_node
         print(node)
+
+
+class singly_linked_list(linked_lists):
+    def __init__(self):
+        super().__init__()
+        self.head = Node(None, first_init=True)
+        self.head.next_node = Node(None, first_init=True)
+
+    def push_back(self, *args):
+        args = list(args)
+        if self.head.data is None:
+            self.head.data = args
+            return None
+        elif self.head.next_node.data is None:
+            self.head.next_node.data = args
+            return None
+        node = self.head
+        while node.next_node is not None:
+            node = node.next_node
+        node.next_node = Node(args)
+
+    def push_front(self, *args):
+        args = list(args)
+        node = Node(args)
+        node.next_node = self.head
+        self.head = node
+
+    def remove(self, *element):
+        element = list(element)
+        node = self.head
+        previous_node = -1
+        while node.data != element and node.next_node is not None:
+            previous_node = node
+            node = node.next_node
+        if node.data == element and node.next_node is None:
+            previous_node.next_node = None
+        if node.data != element and node.next_node is None:
+            raise KeyError
+        if previous_node == -1:
+            self.head = self.head.next_node
+            return
+        previous_node.next_node = node.next_node
+        return
+
+
+class doubly_linked_list(linked_lists):
+    def __init__(self):
+        super().__init__()
+        self.head = Node(None, first_init=True)
+        self.head.previous_node = None
+        self.head.next_node = Node(None, first_init=True)
+        self.head.next_node.previous_node = self.head
+        self.tail = self.head.next_node
+
+    def push_back(self, *args):
+        args = list(args)
+        if self.head.data is None:
+            self.head.data = args
+            return None
+        elif self.head.next_node.data is None:
+            self.head.next_node.data = args
+            return None
+        node = self.head
+        while node.next_node is not None:
+            node = node.next_node
+        node.next_node = Node(args)
+        node.next_node.previous_node = node
+        self.tail = node.next_node
+
+    def push_front(self, *args):
+        args = list(args)
+        node = Node(args)
+        node.next_node = self.head
+        self.head = node
+        self.head.next_node.previous_node = node
+        self.head.previous_node = None
+
+    def remove(self, *element):
+        element = list(element)
+        node = self.head
+        previous_node = -1
+        while node.data != element and node.next_node is not None:
+            previous_node = node
+            node = node.next_node
+        if node.data == element and node.next_node is None:
+            self.tail = previous_node
+            previous_node.next_node = None
+            return
+        if node.data != element and node.next_node is None:
+            raise KeyError
+        if previous_node == -1:
+            self.head = self.head.next_node
+            self.head.previous_node = None
+            return
+        previous_node.next_node = node.next_node
+        node.next_node.previous_node = previous_node
+        return
