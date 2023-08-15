b65d8ff7f4e0f52ab07ae402d0fd3d0e88eb9218
vvrodin <vova.rodin@bk.ru>
2023-08-15 20:51:34 +0300

    second commit

--- Algorithms and data structure/Trie data structure.py
+++ Algorithms and data structure/Trie data structure.py
@@ -24,17 +24,25 @@ class Trie:
         if key == '':
             raise KeyError
 
-    def __find_node(self, key, if_not=False):
+    def __find_node(self, key, if_not=False, remove=False):
         node = self.head
         for i in range(len(key)):
             symbol = key[i]
             if symbol in [t.symbol for t in node.next_nodes]:
+                previous = node
                 for j in node.next_nodes:
                     if j.symbol == symbol:
                         node = j
+                if i == len(key) - 1 and remove:
+                    node.data = None
+                    if not node.next_nodes:
+                        previous.next_nodes.pop(previous.next_nodes.index(node))
+                        return
                 if i == len(key) - 1:
                     return node
             else:
+                if remove:
+                    raise  KeyError
                 if if_not:
                     return -1
                 if i == len(key) - 1:
@@ -60,11 +68,15 @@ class Trie:
         else:
             return node.data
 
+    def remove(self, key):
+        self.__key_validation(key)
+        self.__find_node(key, remove=True)
+
     def __show_trie(self, node, s=''):
         s += node.symbol
         if node.next_nodes:
             if node.data:
-                r = f"'{s}' : {node.data}; "
+                r = f"'{s}': {node.data}, "
                 for j in node.next_nodes:
                     r += self.__show_trie(j, s)
                 return r
@@ -74,13 +86,20 @@ class Trie:
                     r += self.__show_trie(j, s)
                 return r
         else:
-            return f"'{s}' : {node.data}; "
+            return f"'{s}': {node.data}, "
 
     def __str__(self):
-        return self.__show_trie(self.head)
+        return '{' + self.__show_trie(self.head) + '}'
 
     def __getitem__(self, item):
         return self.find(item)
 
     def __setitem__(self, key, value):
         return self.add(key=key, data=value)
+
+import random
+d = Trie()
+for i in ['abc', 'abd', 'bcc', 'cda']:
+    d.add(key=i, data=[1, 2, 3])
+d.remove(key='abc')
+print(d)
